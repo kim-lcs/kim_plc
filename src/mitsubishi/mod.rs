209ff6b9bee868a5eb7e3554863a12d6a3897088
li chengsi <lichengsi2009@126.com>
2024-12-11 23:21:51 +0800

    1. 优化：尝试优化使得IPLC可以共用。

--- src/mitsubishi/mod.rs
+++ src/mitsubishi/mod.rs
@@ -8,6 +8,6 @@ use crate::{core::PlcConnector, IPlc};
 use std::time::Duration;
 
 /// 创建一个 三菱 网口PLC (MC协议 二进制)
-pub fn new_mc_3e_binary_tcp_plc(conn: PlcConnector, timeout: Duration) -> Mc3eBinaryTcpPlc {
+pub fn new_mc_3e_binary_tcp_plc(conn: PlcConnector, timeout: Duration) -> impl IPlc {
     Mc3eBinaryTcpPlc::new(conn, timeout)
 }
--- src/panasonic/mod.rs
+++ src/panasonic/mod.rs
@@ -8,6 +8,6 @@ use crate::{core::PlcConnector, IPlc};
 use std::time::Duration;
 
 /// 创建一个松下 网口PLC (Newtocol协议)
-pub fn new_newtocol_tcp_plc(conn: PlcConnector, timeout: Duration) -> NewtocolTcpPlc {
+pub fn new_newtocol_tcp_plc(conn: PlcConnector, timeout: Duration) -> impl IPlc {
     NewtocolTcpPlc::new(conn, timeout)
 }
